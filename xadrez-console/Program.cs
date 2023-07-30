2686fc5d93e3ff99e95b20a121197a1524788333
Pedro Firmiano <pedro_firmiano@hotmail.com>
2023-07-30 18:14:36 -0300

    Implementando restrições de movimentos

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -18,35 +18,53 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
 
                 PartidaDeXadrez partida = new PartidaDeXadrez();
-                
+
 
                 while (!partida.terminada)
                 {
-                    Console.Clear();
-                    Tela.imprimirTabuleiro(partida.tab);
-                    Console.WriteLine();
-                    Console.Write("Origem ");
-                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    try
+                    {
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab);
+                        Console.WriteLine();
+                        Console.WriteLine("Turno " + partida.turno);
+                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
 
 
-                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPosiveis();
-
-                    Console.Clear();
-                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
 
 
-                    Console.Write("Destino ");
-                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        Console.WriteLine();
+                        Console.Write("Origem ");
+                        Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
 
 
-                    partida.executaMovimento(origem, destino);
+
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPosiveis();
+
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+
+                        Console.Write("Destino ");
+                        Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeDestino(origem, destino);
+
+                        partida.realizaJogada(origem, destino);
+
+                    }
+                    catch (TabuleiroException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
+
+
+
 
                 }
-                
-
-
             }
-            catch(TabuleiroException e) 
+            catch (TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
             }
--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -10,8 +10,8 @@ namespace xadrez_console.xadrez
     internal class PartidaDeXadrez
     {
         public Tabuleiro tab { get; private set; }
-        private int turno;
-        private Cor jogadorAtual;
+        public int turno { get; private set; }
+        public Cor jogadorAtual { get; private set; }
         public bool terminada { get; private set; } 
 
 
@@ -32,6 +32,53 @@ namespace xadrez_console.xadrez
             tab.colocarPeca(p, destino);
 
 
+        }
+
+        public void validarPosicaoDeOrigem(Posicao pos)
+        {
+            if (tab.peca(pos) == null) { 
+                throw new TabuleiroException(" Não existe peça na posição de origem escolhida");
+            }
+
+            if (jogadorAtual != tab.peca(pos).cor)
+            {
+                throw new TabuleiroException(" Peça de origem escolhida não é sua");
+
+            }
+
+            if (!tab.peca(pos).existeMovimentosPossiveis()) {
+                throw new TabuleiroException("Não há movimentos possiveis para a peça de origem escolhida!");
+            }
+        }
+
+        public void validarPosicaoDeDestino(Posicao origem, Posicao destino)
+        {
+            if (!tab.peca(origem).podeMoverPara(destino)) {
+                throw new TabuleiroException("Posição de destino invalida");
+            }
+
+
+        }
+
+
+        public void realizaJogada(Posicao origem, Posicao destino)
+        {
+            executaMovimento(origem, destino);
+            turno ++;
+            mudaJogador();
+        }
+
+        private void mudaJogador()
+        {
+            if (jogadorAtual == Cor.Branca)
+            {
+                jogadorAtual = Cor.Preta;
+            }
+            else
+            {
+                jogadorAtual = Cor.Branca;
+
+            }
         }
 
         private void colocarPecas()
